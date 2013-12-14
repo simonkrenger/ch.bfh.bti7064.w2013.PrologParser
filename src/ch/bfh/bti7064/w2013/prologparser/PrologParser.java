2c85b6ceaf4262e3d504f12f2dc1f81ad5b9a797
Simon Krenger <simon@krenger.ch>
2013-12-14 13:51:23 +0100

    Initialised Java Project, added BNF grammar of Prolog

---
+++ src/ch/bfh/bti7064/w2013/prologparser/PrologParser.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ch.bfh.bti7064.w2013.prologparser;
+
+/**
+ * @author simon
+ *
+ */
+public class PrologParser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
